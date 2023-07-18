2713d45ac11dab0c4278eb56d8ab5e6ea807d6b0
omarebid1 <omar3bid18@gmail.com>
2023-07-18 18:46:40 +0200

    Classe Main finito

--- src/org/lessons/java/geometria/Main.java
+++ src/org/lessons/java/geometria/Main.java
@@ -1,12 +1,23 @@
 package org.lessons.java.geometria;
 
+import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args) {
 
-        Rettangolo rec = new Rettangolo(4, 20);
+        Scanner scan = new Scanner(System.in);
 
+        System.out.println("inserisci la base!");
+        int base = scan.nextInt();
+
+        System.out.println("inserisci l'altezza!");
+        int altezza = scan.nextInt();
+
+        Rettangolo rec = new Rettangolo(base, altezza);
+
+        System.out.println("Area : " + rec.calcolaArea());
+        System.out.println("Perimetro : " + rec.calcolaPerimetro());
 
     }
 
