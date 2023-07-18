31a002d9aad0c4f6d64f6fc63247821e58f68681
omarebid1 <omar3bid18@gmail.com>
2023-07-18 18:33:58 +0200

    file creato

---
+++ src/org/lessons/java/geometria/Rettangolo.java
@@ -0,0 +1,18 @@
+package org.lessons.java.geometria;
+
+public class Rettangolo {
+
+    private
+        int base, altezza;
+
+
+    public Rettangolo (int base, int altezza){
+
+        this.altezza = altezza;
+        this.base = base;
+
+    }
+
+
+
+}
